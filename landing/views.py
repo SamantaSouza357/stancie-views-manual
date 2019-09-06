9d6b08e8f4a2ac495a4064c3e3261add43be40ab
samantasouza357 <samantasouza931@gmail.com>
2019-09-06 15:32:46 -0300

    adicionar filter

--- landing/views.py
+++ landing/views.py
@@ -1,5 +1,6 @@
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.filters import SearchFilter
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 from landing.models import Aluno
@@ -10,6 +11,8 @@ from landing.serializers import AlunoSerializer
 
 
 class AlunoViewset(viewsets.ModelViewSet):
+    filter_backends = [SearchFilter]
+    search_fields = ['^nome','^idade','=email']
     queryset = Aluno.objects.all()
     permission_classes = (IsAuthenticatedOrReadOnly,)
     authentication_classes = (TokenAuthentication,)
